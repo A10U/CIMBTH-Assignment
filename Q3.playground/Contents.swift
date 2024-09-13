b88cdc60e467985519ae86d0ef225bc423e7ba35
Teerawat Sethsathien <12.03.1985.Au@gmail.com>
2024-09-13 19:26:43 +0700

    update answerQ3

--- Q3.playground/Contents.swift
+++ Q3.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func climbingStaircase(step : Int) -> Int {
+    var tempA = 0
+    var tempB = 1
+    
+    for _ in (0..<step) {
+        let tempC = tempA + tempB
+        tempA = tempB
+        tempB = tempC
+    }
+    
+    return tempB
+}
+
+//Example 1:
+//Input
+let ex1N = 2
+//Output
+climbingStaircase(step: ex1N)
+
+//Example 2:
+//Input:
+let ex2N = 3
+//Output:
+climbingStaircase(step: ex2N)
