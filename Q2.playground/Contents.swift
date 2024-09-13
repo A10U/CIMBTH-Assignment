b07b42ac6a57ac7130e18f4f619f8ae2d667e210
Teerawat Sethsathien <12.03.1985.Au@gmail.com>
2024-09-13 19:46:34 +0700

    update Q2

--- Q2.playground/Contents.swift
+++ Q2.playground/Contents.swift
@@ -1,36 +1,28 @@
 import UIKit
 
 func compareVersion(version1: String, version2: String) -> Int{
-    let arrV1 = version1.split(separator: ".")
-    let arrV2 = version2.split(separator: ".")
     
-    var i = 0
+    // Split "." and map to Int value of array
+    let arrV1 = version1.split(separator: ".").map { Int($0)! }
+    let arrV2 = version2.split(separator: ".").map { Int($0)! }
     
-    let arrV1Count = arrV1.count
-    let arrV2Count = arrV2.count
+    // compare to get max lenght
+    let maxLength = max(arrV1.count, arrV2.count)
     
-    while i < arrV1Count || i < arrV2Count {
+    for i in 0..<maxLength {
+        //get part of version
+        let partOfV1 = i < arrV1.count ? arrV1[i] : 0
+        let partOfV2 = i < arrV2.count ? arrV2[i] : 0
         
-        if i < arrV1Count && i < arrV2Count {
-            if Int(arrV1[i])! < Int(arrV2[i])! {
-                return -1
-            }else if Int(arrV1[i])! > Int(arrV2[i])! {
-                return 1
-            }
-        } else if i < arrV1Count {
-            if Int(arrV1[i])! != 0 {
-                return 1
-            }
-        } else if i < arrV2Count {
-            if Int(arrV2[i])! != 0 {
-                return -1
-            }
+        //compare part of value
+        if partOfV1 < partOfV2 {
+            return -1
+        } else if partOfV1 > partOfV2 {
+            return 1
         }
-        
-        i += 1
     }
     
-    return 0
+    return 0 //if they are equal
     
 }
 
